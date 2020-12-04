31e54934c7a39b711fb9f8b14551f56a96c21a61
KIm yeong hyun <gus7518@naver.com>
2020-12-04 16:26:39 +0900

    weather namechange

--- src/main/java/com/PCorder/App.java
+++ src/main/java/com/PCorder/App.java
@@ -1,15 +1,13 @@
 package com.PCorder;
 
-import Controller.AccountChecker;
 import Controller.C_AdminView;
 import Controller.C_ProdManager;
 import Controller.C_SignUp;
 import Controller.C_UserView;
 import Controller.C_login;
 import Controller.PCController;
-import Model.CustomersDao;
+import Model.Customers_DAO;
 import Model.PCChatData;
-import Model.ViewState;
 import View.AdminView;
 import View.CusManager;
 import View.GUIView;
@@ -21,8 +19,9 @@ import View.SignUpView;
 public class App 
 {
 	public static PCController app;
-	public static CustomersDao dao;
+	public static Customers_DAO dao;
 	public static void main(String[] args) {
+		
 		System.out.println("App.java에서 프로그램을 시작합니다...");
 		
 		////////////////////////////////////////////////////
@@ -30,29 +29,19 @@ public class App
 		// -> Controller\PCServer.java
 		// -> com.PCorder\App.java
 		////////////////////////////////////////////////////
-		
-		LoginView loginView = LoginView.getInstance();
-		SignUpView signUpView = SignUpView.getInstance();
-		GUIView guiView = GUIView.getInstance();
-		CusManager cusManager = CusManager.getInstance();
-		ProdManager prodManager = ProdManager.getInstance();
-		AccountChecker accountChecker = AccountChecker.getInstance();
 				
 		//컨트롤러 객체 생성
 		app = new PCController	(
-								loginView, cusManager, 
-								prodManager, guiView,
-								new AdminView(), signUpView,
-								new C_login(), new C_SignUp(),
+								LoginView.getInstance(), CusManager.getInstance(), 
+								ProdManager.getInstance(), GUIView.getInstance(),
+								AdminView.getInstance(), SignUpView.getInstance(), 
+								new C_login(), new C_SignUp(), 
 								new C_ProdManager(), new C_UserView(),
 								new C_AdminView(), new PCChatData(), 
 								new PCChatData()
 								);
 		//고객 DAO객체 생성
-		dao = CustomersDao.getInstance();
-
-		// viewState 활성화
-		ViewState.getInstance();
+		dao = Customers_DAO.getInstance();
 		
 		app.appMain();
 	}
